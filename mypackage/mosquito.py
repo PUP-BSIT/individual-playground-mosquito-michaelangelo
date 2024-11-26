3835ebf8e875cb450ddfc87de093d058f5af4782
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2024-11-26 16:18:11 +0800

    fix mosquito.py

--- mypackage/mosquito.py
+++ mypackage/mosquito.py
@@ -6,12 +6,9 @@ def date_function():
     date_now = date.today()
     next_day = D.tomorrow()
     last_day = D.yesterday()
-    days_7_from_now = D.today() + relativedelta(days=7)
 
     print("This is the date today:",date_now)   
 
     print("This is the date tomorrow:",next_day)
 
     print("This is the date yesterday:", last_day)
-
-    print("This is the date 7 days from now:", days_7_from_now)
